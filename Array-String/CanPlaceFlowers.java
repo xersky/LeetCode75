d378e19333a699d8375a23b75b23949e254d0529
xersky <ayoub.elaoufi@gmail.com>
2023-09-18 17:34:47 +0100

    day 4 - done

--- Array-String/CanPlaceFlowers.java
+++ Array-String/CanPlaceFlowers.java
@@ -23,12 +23,26 @@ There are no two adjacent flowers in flowerbed.
 
 public class CanPlaceFlowers {
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(flowerbed.length == 1 && flowerbed[0] == 0) return true;
 
-        return true;
+        boolean potentialPlantedFlower = true;
+        int possibleAdjacentFlowersNumber = 0;
+
+        for(int i = 0; i < flowerbed.length - 1; i++) {
+                if(flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                    if(potentialPlantedFlower || i == flowerbed.length - 1) {
+                        possibleAdjacentFlowersNumber++;
+                        potentialPlantedFlower = false;
+                    } else potentialPlantedFlower = true;
+                } else potentialPlantedFlower = false;
+        }
+        
+        if(possibleAdjacentFlowersNumber >= n) return true;
+        else return false;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] flowerbed = {1, 0, 0, 0, 1};
-        System.out.println(canPlaceFlowers(flowerbed, 2));
+        System.out.println(canPlaceFlowers(flowerbed, 1));
     }
 }
