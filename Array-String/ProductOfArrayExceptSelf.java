252f33405287cf18d92c3594e1aed920bd4ecc86
xersky <ayoub.elaoufi@gmail.com>
2023-09-21 12:05:25 +0100

    day 6 - done

--- Array-String/ProductOfArrayExceptSelf.java
+++ Array-String/ProductOfArrayExceptSelf.java
@@ -27,5 +27,57 @@ Follow up: Can you solve the problem in O(1) extra space complexity? (The output
  */
 
 public class ProductOfArrayExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
 
+        int[] answer = new int[nums.length];
+
+        int productOfAll = 1;
+        int numOfZeros = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) numOfZeros++;
+            else productOfAll *= nums[i];
+
+            if(numOfZeros >= 2) break;
+        }
+
+        if(numOfZeros >= 2){
+            for(int i = 0; i < answer.length; i++) answer[i] = 0;
+        } else if(numOfZeros == 1) {
+            for(int i = 0; i < answer.length; i++) {
+                if (nums [i] != 0) answer[i] = 0;
+                else answer[i] = productOfAll;
+            }
+        } else if((productOfAll == 1 || productOfAll == -1)){
+            for(int i = 0; i < answer.length; i++) answer[i] = nums[i] * productOfAll;
+        } else {
+            /* for(int i = 0; i < nums.length; i++) {
+                answer[i] = 1;
+                for(int j = 0; j < nums.length; j++) {
+                    if(i != j) answer[i] *= nums[j];
+                }
+            } */
+            int productParser = 1;
+            answer[0] = 1;
+            for(int i = 1; i < nums.length; i++) {
+                productParser *= nums[i - 1];
+                answer[i] = productParser;
+            }
+            productParser = 1;
+            for(int i = nums.length - 1; i >= 0; i--) {
+                answer[i] *= productParser;
+                productParser *= nums[i];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(productExceptSelf(nums));
+    }
 }
+
+
+
+
