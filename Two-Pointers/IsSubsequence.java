13815673a6cc0420f96b912c44d5cc05638c5342
xersky <ayoub.elaoufi@gmail.com>
2023-09-27 10:49:14 +0100

    day 10 - done

--- Two-Pointers/IsSubsequence.java
+++ Two-Pointers/IsSubsequence.java
@@ -24,10 +24,19 @@ class IsSubsequence {
 
     public static boolean isSubsequence(String s, String t) {
 
+        int counter = 0;
+
+        if(s.length() < 1) return true;
+
+        for(int i = 0; i < t.length(); i++) {
+            if(s.charAt(counter) == t.charAt(i)) counter++;
+            if(counter == s.length()) return true;
+        }
+        
         return false;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(isSubsequence("lol", "lol"));
     }
 }
