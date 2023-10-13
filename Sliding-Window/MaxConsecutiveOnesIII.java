fda26cbe475c185dc6bc26a0c4627a13e27755d8
xersky <ayoub.elaoufi@gmail.com>
2023-10-13 12:45:03 +0100

    day 15 - done (but it feel there is a simpler approach)

--- Sliding-Window/MaxConsecutiveOnesIII.java
+++ Sliding-Window/MaxConsecutiveOnesIII.java
@@ -23,7 +23,44 @@ nums[i] is either 0 or 1.
 0 <= k <= nums.length */
 
 public class MaxConsecutiveOnesIII {
-    public int longestOnes(int[] nums, int k) {
-        return 0;
+    public static int longestOnes(int[] nums, int k) {
+
+        int max = 0;
+        int count = 0;
+        boolean isConsecutive = true;
+        int startingPoint = 0;
+        int flipper = k;
+        int i = 0;
+
+        while(i < nums.length) {
+            
+            if(isConsecutive) {
+                isConsecutive = false;
+                startingPoint = i;
+            }
+
+            if(nums[i] == 1) count++;
+            else if(nums[i] == 0 && flipper > 0) {
+                count++;
+                flipper--;
+            } else if(nums[i] == 0 && flipper == 0) {
+                if (max < count) max = count;
+                flipper = k;
+                count = 0;
+                i = startingPoint;
+                isConsecutive = true;
+            }
+
+            if(max < count) max = count;
+
+            i++;
+        }
+        
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums= {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+        System.out.println(longestOnes(nums, 3));
     }
 }
