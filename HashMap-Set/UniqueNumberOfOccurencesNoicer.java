4476bc8005e7b222ad16cb037f2fcdebdc00d1c9
xersky <ayoub.elaoufi@gmail.com>
2024-01-24 12:30:46 +0100

    21 enhanced using hashmap and set

---
+++ HashMap-Set/UniqueNumberOfOccurencesNoicer.java
@@ -0,0 +1,42 @@
+/* Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000 */
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueNumberOfOccurencesNoicer {
+    public static boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer> mapOfOccurences = new HashMap<Integer,Integer>();
+        for (int elem : arr) mapOfOccurences.put(elem, mapOfOccurences.getOrDefault(elem, 0) + 1);
+        Set<Integer> setOfOccurences = new HashSet<Integer>(mapOfOccurences.values());
+        return mapOfOccurences.size() == setOfOccurences.size();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-3,0,1,-3,1,1,1,-3,10,0};
+        System.out.println(uniqueOccurrences(arr));
+    }
+}
