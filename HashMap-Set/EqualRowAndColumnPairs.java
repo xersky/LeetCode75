e9bfeae05e2c6c1f20ebb8f41683ec313931eaf7
xersky <ayoub.elaoufi@gmail.com>
2024-01-27 20:31:23 +0100

    24 done

--- HashMap-Set/EqualRowAndColumnPairs.java
+++ HashMap-Set/EqualRowAndColumnPairs.java
@@ -14,7 +14,12 @@ Explanation: There is 1 equal row and column pair:
 Example 2:
 
 
-Input: grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]
+Input: grid = [
+    [3,1,2,2],
+    [1,4,4,5],
+    [2,4,2,2],
+    [2,4,2,2]
+]
 Output: 3
 Explanation: There are 3 equal row and column pairs:
 - (Row 0, Column 0): [3,1,2,2]
@@ -28,8 +33,38 @@ n == grid.length == grid[i].length
 1 <= n <= 200
 1 <= grid[i][j] <= 105 */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class EqualRowAndColumnPairs {
-    public int equalPairs(int[][] grid) {
-        return 1;
+    public static int equalPairs(int[][] grid) {
+        int n = grid.length;
+        int counter = 0 ;
+
+        List<int[]> columns = new ArrayList<int[]>();
+        List<int[]> rows = new ArrayList<int[]>();
+
+        for(int i = 0; i < n; i++) {
+            int[] column = new int[n];
+            for(int j = 0; j < n; j++) {
+                column[j] = grid[j][i];
+            }
+            columns.add(column);
+            rows.add(grid[i]);
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(Arrays.equals(rows.get(i), columns.get(j))) counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{3,1,2,2},{1,4,4,5},{2,4,2,2},{2,4,2,2}};
+        System.out.println(equalPairs(grid));
     }
 }
