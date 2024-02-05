ec05b46deaacc86c4ad7a2421345f33761af4f6d
xersky <ayoub.elaoufi@gmail.com>
2024-02-06 00:14:47 +0100

    28 done

--- Queue/RecentCounter.java
+++ Queue/RecentCounter.java
@@ -35,26 +35,23 @@ import java.util.Queue;
 
 public class RecentCounter {
     Queue<Integer> requests = new LinkedList<>();
-    int counter;
-
 
     public RecentCounter() {
-        this.requests.removeAll(requests);
-        this.counter = 0;
+        this.requests.clear();
     }
     
     public int ping(int t) {
-        int rangeLeft = t - 3000;
-        int rangeRight = t;
-
+        int counter = 0;
         requests.add(t);
-
-        for (Integer integer : requests) {
-            if(integer <= rangeRight && integer >= rangeLeft) this.counter++;
-        }
-
-        return this.counter;
-
+        for (Integer integer : requests) if(integer <= t - 3000 && integer >=  t) counter++;
+        return counter;
+    }
+    public static void main(String[] args) {
+        RecentCounter recentCounter = new RecentCounter();
+        System.out.println(recentCounter.ping(1));     // requests = [1], range is [-2999,1], return 1
+        System.out.println(recentCounter.ping(100));   // requests = [1, 100], range is [-2900,100], return 2
+        System.out.println(recentCounter.ping(3001));  // requests = [1, 100, 3001], range is [1,3001], return 3
+        System.out.println(recentCounter.ping(3002));  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
     }
 }
 
